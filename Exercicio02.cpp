e5c2829a0252892f567fcb9cffcad936e81facbe
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2020-09-14 16:31:34 -0300

    Add files via upload

---
+++ Exercicio02.cpp
@@ -0,0 +1,21 @@
+/* No Código do exercício 02, utilize os atalhos do visual Sudio Community 2019/2017 para:
+° Criar um comentário de uma linha de código com //
+colocando seu nome completo e data de criação do programa.
+° Criar um comentário de várias linhas de código
+/**/ 
+/*Explicando e descrevendo o que faz e qual objetivo deste programa
+° Auimentar e Diminuit o texto de seu CódigoFonte*/
+
+#include < iostream>
+// Aluno DanielDlc
+// data: 14 de setembro de 2020
+
+int main() // indica que iniciou uma função com parametro main.
+{
+	// indica a saída de dados com a mensagem Meu primeiro programa...
+	std::cout << "Meu primeiro programa utilizando a linguagem C++" << std::endl;
+	
+	// retorno zero indica que o programa foi finalizado com sucesso.
+	return 0;
+
+	}
