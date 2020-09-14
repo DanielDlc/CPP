a3e1c4c5ac9a9893c9ca3efaf33c251a9829a30e
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2020-09-14 16:31:11 -0300

    Exercícios C Plus Plus

---
+++ Exercicio01.cpp
@@ -0,0 +1,18 @@
+/* Altere o programa do exercício oi para que cada palavra da frase:
+Vou aprender a programar na Linguagem C++
+apareça em linhas diferentes*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Vou" << std::endl;
+	std::cout << "aprendedr" << std::endl;
+	std::cout << "a" << std::endl;
+	std::cout << "programar" << std::endl;
+	std::cout << "na" << std::endl;
+	std::cout << "linguagem" << std::endl;
+	std::cout << "C++" << std::endl;
+	
+	return 0;
+}
