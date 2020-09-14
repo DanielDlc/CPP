3e4c9e24fc0734298e84bd1f9f3e94fb21afbd24
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2020-09-14 16:37:15 -0300

    Update Exercicio03.cpp

--- Exercicio03.cpp
+++ Exercicio03.cpp
@@ -1,23 +1,23 @@
-/* Trocar cor Utilizando o comazndo System(COLOR)
-e altere a cor de exibição da fonte no terminal de saída para uma cor de sua preferência.*/
+/* Trocar a cor Utilizando o comando System(COLOR)
+e altere a cor de exibiÃ§Ã£o da fonte no terminal de saÃ­da para uma cor de sua preferÃªncia.*/
 
 #include <iostream>
 
  int main()
  {
-	 /* Cores e letras disponíveis:
+	 /* Cores e letras disponÃ­veis:
 	 0 = Preto						8 = Cinza							
 	 1 = Azul						9 = Azul claro
-	 3 = Verde-água					A = Verde claro
-	 4 = Vermelho					B = Verde-água claro			F = Branco brilhante
+	 3 = Verde-Ã¡gua					        A = Verde claro
+	 4 = Vermelho					        B = Verde-Ã¡gua claro			F = Branco brilhante
 	 5 = Roxo						C = Vermelho claro
-	 6 = Amarelo					D = Lilás
-	 7 = Branco					    E = Amarelo claro */
+	 6 = Amarelo					        D = LilÃ¡s
+	 7 = Branco					        E = Amarelo claro */
 
-	 // de 0 até F, o 1° modifica o fundo e o segundo modifica a letra.
-	 system("COLOR 4E"); // color 4 (Fundo Lilás), seguido de E (Letra amarela)
+	 // de 0 atÃ© F, o 1Â° modifica o fundo e o segundo modifica a letra.
+	 system("COLOR 4E"); // color 4 (Fundo LilÃ¡s), seguido de E (Letra amarela)
 
 	 std::cout << "Vamos modificar as cores deste programa " << std::endl;
 	 
-	 return 0;
+         return 0;
  }
