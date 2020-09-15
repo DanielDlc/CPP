0ae1c681a59e3c79571e5a6242d3598a1acdb25f
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2020-09-14 21:52:34 -0300

    Update and rename PrimeiroProgramacpp.cpp to PrimeiroPrograma00.cpp

---
+++ PrimeiroPrograma00.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // permite que o programa gere saída de dados na tela
+#include <cmath> // biblioteca de matemática C++ padrão
+
+// a função main inicia a execução
+int main()
+{
+	std::sout << "Bem vindo ao mundo de C++ " std::endl; // exibe a mensagem
+	
+	return 0; // indica que o programa terminou com sucesso
+} // fim da função main
