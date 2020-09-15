8b8a607109dd158a93298742d1cf105c53f5e2ee
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2020-09-14 22:08:15 -0300

    Update PrimeiroPrograma00.cpp

--- PrimeiroPrograma00.cpp
+++ PrimeiroPrograma00.cpp
@@ -1,12 +1,13 @@
 // Primeiro programa C ++
 // Daniel Louro Costa
 
-#include <iostream> // permite que o programa gere saída de dados na tela
+// permite que o programa gere saída de dados na tela
+#include <iostream>
 
 // a função main inicia a execução
 int main()
 {
-	std::sout << "Bem vindo ao mundo de C++ " std::endl; // exibe a mensagem
+	std::cout << "Ola mundo C++ " << std::endl; // exibe a mensagem
 	
 	return 0; // indica que o programa terminou com sucesso
 } // fim da função main
