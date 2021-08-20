a7903d7ede2b6cf94c8482542a3b688ad1ff5566
DanielDlc <dlc.engcomputacao@gmail.com>
2021-08-20 07:37:24 -0300

    condicionais

---
+++ 02_condicionais/01_if.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool meia_noite = false;
+    bool jogo_zerado = false;
+
+    if (meia_noite != true)
+    {
+        if (jogo_zerado != true)
+        {
+            cout << "Vou continuar jogando video game \n";
+        }
+    }
+    return 0;
+}
---
+++ 02_condicionais/02_fi_else.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // instruções if simples com else
+    int i = 2;
+    if (i > 2) {
+        std::cout << i << "é maior que 2 \n";
+    } else {
+        std::cout << i << "não é maior que 2 \n";
+    }
+    return 0;
+}
