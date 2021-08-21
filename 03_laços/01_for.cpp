14a8b297e6027b23be7e4c007a419bbf125fd784
DanielDlc <dlc.engcomputacao@gmail.com>
2021-08-20 21:57:40 -0300

    `contagem até 10 usando for

---
+++ 03_laços/01_for.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// dontador de 0 até 10
+int main() 
+{
+    int contagem;
+    for(contagem = 0; contagem <= 10; ++contagem) {
+        std::cout << contagem << std::endl;
+    }
+    return 0;
+}
