e2069e3016bd80c0306333d4a22cd95787235aa9
DanielDlc <dlc.engcomputacao@gmail.com>
2021-08-20 21:57:52 -0300

    contagem até 10 com while

---
+++ 03_laços/02_while.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// contar até 10
+int main() 
+{
+    int homem = 0;
+    while(homem < 10) {
+        homem++;
+        std::cout << "Homem n:" << homem << std::endl;
+    }
+    return 0;
+}
