0027ae23896cb10a8727140b5ec1137fd9796237
DanielDlc <dlc.engcomputacao@gmail.com>
2021-08-21 10:17:47 -0300

    matriz  bidimensional adicionando valores com for

---
+++ 04_matrizes/03_matriz_bidimensional.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// matriz bidimensional
+int main()
+{
+    int matriz[3][4];
+    int i,j;
+
+    // adicionando valores na matriz
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 4; j++) {
+            matriz[i][j] = i;
+        }
+    }
+
+    // iteração para percorrer a matriz
+    for(i = 0; i < 3; i++) {
+        for(j = 0; j < 4; j++) {
+            cout << matriz[i][j] <<" ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
