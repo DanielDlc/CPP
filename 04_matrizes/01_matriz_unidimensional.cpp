e61e5abd92648cc27745daf5f0e724fe6da981de
DanielDlc <dlc.engcomputacao@gmail.com>
2021-08-21 10:14:07 -0300

    matriz unidimensional

---
+++ 04_matrizes/01_matriz_unidimensional.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// contar 10 filmes usando matriz unidimensional
+int main()
+{
+    string filmes[10] = {"filme01", "filme02", "filme03", "filme04", "filme05",
+                        "filme06", "filme07", "filme08", "filme09", "filme10"};
+    
+    int i;
+    for(i = 0; i < 10; i++) {
+        cout << filmes[i] << "\n";
+    }
+    return 0;
+}
