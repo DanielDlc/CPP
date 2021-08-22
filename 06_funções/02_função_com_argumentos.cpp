b8b4e8d8e3b3411a9b87db6bb365b2c94fe45422
DanielDlc <dlc.engcomputacao@gmail.com>
2021-08-22 08:52:19 -0300

    função com parâmetro e argumento

---
+++ 06_funções/02_função_com_argumentos.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+// função com argumento subtrair
+void subtrair(int numero01, int numero02);
+
+// atribuindo valor01 no parâmetro numero01 e valor02 no parâmetro numero02
+int main()
+{
+    int valor01 = 600;
+    int valor02 = 200;
+    subtrair(valor01, valor02);
+    return 0;
+}
+
+void subtrair(int numero01, int numero02)
+{
+    int resultado = numero01 - numero02;
+    std::cout << "o resultado da subtração é: " << resultado << std::endl;
+}
