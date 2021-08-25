e97e2250fe1c4655e4ad8c48ba4e8840a5f0abdc
DanielDlc <dlc.engcomputacao@gmail.com>
2021-08-24 22:33:13 -0300

    printf usando C++

---
+++ 09_entrada_e_saída/01_printf.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+// criando um programa usando printf
+int main()
+{
+    printf("\nEssa mensagem foi escrita com printf\n");
+    printf("------------------------------------\n");
+    cout << "Essa mensagem foi escrita com cout\n\n";
+
+    return 0;
+}
