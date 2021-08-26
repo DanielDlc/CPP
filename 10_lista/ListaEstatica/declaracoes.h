dd4e16198ca02dd16fd123612b3393d46d4fbbe5
DanielDlc <dlc.engcomputacao@gmail.com>
2021-08-26 07:04:34 -0300

    declarações de lista estática

---
+++ 10_lista/ListaEstatica/declaracoes.h
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// 
+struct Produto{
+    int qtd;
+    string nome;
+};
+
+struct Lista{
+    int qtd;
+    int primeiro;
+    int ultimo;
+};
+
+Lista *l;
+const int tamanho = 10;
