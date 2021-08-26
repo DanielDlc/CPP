e6fc7d2bcb1b52c6db0ce0af9029e2bae4d4f344
DanielDlc <dlc.engcomputacao@gmail.com>
2021-08-26 07:29:11 -0300

    inicialização de produtos e informação sobre lista cheia ou vazia

--- 10_lista/ListaEstatica/funcoes.h
+++ 10_lista/ListaEstatica/funcoes.h
@@ -2,3 +2,35 @@
 #include "declaracoes.h"
 using namespace std;
 
+void inicializarProdutos(Produto produtos[]){
+    for(int i = 0; i < tamanho; i++) {
+        produtos[i].nome = "";
+        produtos[i].qtd = 0;
+    }
+}
+
+void imprime(Produto produtos[]){
+    for (int i = 0; i < tamanho; i++) {
+        if (produtos[i].qtd > 0){
+            cout << "i: " << i << " - nome:" << produtos[i].nome << " - qtd: " << produtos[i].qtd <<endl;
+        }
+    }
+}
+
+bool listaCheia(){
+    if (l->qtd == tamanho){
+        cout << "A lista esta cheia" << endl;
+        return true;
+    }
+    else
+        return false;
+}
+
+bool listaVazia(){
+    if (l->qtd == 0){
+        cout << "A lista esta vazia" << endl;
+        return true;
+    }
+    else
+        return false;    
+}   
