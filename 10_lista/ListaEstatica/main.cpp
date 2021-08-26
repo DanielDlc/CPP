48dd54a0312d8ea65b1b0badafa7cbcc3a090027
DanielDlc <dlc.engcomputacao@gmail.com>
2021-08-26 07:05:49 -0300

    lista estática

---
+++ 10_lista/ListaEstatica/main.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <iostream>
+#include "funcoes.h"
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    Produto *produtos = new Produto{tamanho};
+    
+    return 0;
+} 
