141399bf8602d7222f723811b59f6978cc26c5b7
DanielDlc <dlc.engcomputacao@gmail.com>
2021-08-26 07:29:48 -0300

    inicializando a lista l

--- 10_lista/ListaEstatica/main.cpp
+++ 10_lista/ListaEstatica/main.cpp
@@ -6,6 +6,11 @@ using namespace std;
 
 int main(int argc, char** argv) {
     Produto *produtos = new Produto{tamanho};
+
+    l = new Lista();
+    l->qtd = 0;
+    l->primeiro = 0;
+    l->ultimo = 0;
     
     return 0;
 } 
