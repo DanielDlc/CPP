ce97ff9ccdde0111a84931544d1fea5bf693d2d0
DanielDlc <dlc.engcomputacao@gmail.com>
2021-08-27 10:57:00 -0300

    exibindo endereço de memória e variável ponteiro

---
+++ 07_ponteiros/02_ponteiro.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+
+    int *ponteiro, valor, variavel;
+    valor = 400;
+    ponteiro = &valor;
+    variavel = *ponteiro;
+
+    std::cout << "Endereco da variavel com um valor: " << &valor << std::endl;
+    std::cout << "Lendo o conteudo do ponteiro: " << ponteiro << std::endl;
+    std::cout << "Endereco da variavel ponteiro: " << &ponteiro << std::endl;
+    std::cout << "Conteudo da variavel apontada pelo ponteiro: " << *ponteiro << std::endl;
+    std::cout << "Conteudo da variavel: " << variavel << std::endl;
+
+    return 0;
+}
