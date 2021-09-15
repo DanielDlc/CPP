94ea24604c489d8d6f3759b960a6bd4ae5e528d7
DanielDlc <dlc.engcomputacao@gmail.com>
2021-09-15 08:08:18 -0300

    lista dinâmica declarações

---
+++ 10_lista/ListaDinamica/declaracoes.h
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+struct produto
+{
+    int qtd;
+    string nome;
+    produto *prox;
+    Produto *ant;
+};
+
+struct lista
+{
+    int qtd;
+    Produto *primeiro;
+    Produto *ultimo;
+};
+
+Lista *l;
