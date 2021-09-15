0af36932c435b66a20ccdb084cce887e1a94196e
DanielDlc <dlc.engcomputacao@gmail.com>
2021-09-15 08:08:47 -0300

    funções da lista dinâmica

---
+++ 10_lista/ListaDinamica/funcoes.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "declaracoes.h"
+using namespace std;
+
+bool listaCheia(){
+    if(l->qtd == limite){
+        cout << "Lista Cheia" << endl;
+        return true;
+    } else
+    return false;
+}   
+
+bool listaVazia(){
+    if(l->qtd == 0) {
+        return true;
+    } else   
+    return false;
+}
+
+void imprimeLista(){
+    if(listaVazia())
+        return;
+    else {
+        produto *p = new Produto;
+        p = l->primeiro;
+        do{
+            cout << "nome: " << p ->nome;
+            cout << " - qtd: " << p->qtd;
+            p = p->prox;
+        }while(p != NULL);
+    }
+}
