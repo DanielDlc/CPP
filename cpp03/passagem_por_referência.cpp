b8a94cebdd1e20cb1b2c267a9bfa636dd0060626
DanielDlc <dlc.engcomputacao@gmail.com>
2021-09-30 06:57:03 -0300

    passagem por referência

---
+++ cpp03/passagem_por_referência.cpp
@@ -0,0 +1,27 @@
+//Passagem por referência, utilizamos  & ( e comercial )
+// podemos fazer uma função operar diretamente sobre qualquer objeto para o
+// qual passamos uma referência
+
+#include <iostream>
+
+int f(int &x)
+{
+    x = x+1;
+    return x;
+}
+
+int main() {
+    
+    int xx= 0;
+    std::cout << f(xx) <<std::endl; // escreve: 1 
+    std::cout << xx <<std::endl;  // escreve: 1; f() modifica o valor xx
+
+    std::cout <<"---------------------------" << std::endl;
+
+    int yy = 7;
+    std::cout << f(yy) <<std::endl; // escreve: 8
+    std::cout << yy <<std::endl;  // escreve: 8; f() modifica o valor yy
+
+    return 0;
+
+}
