84e09d01d8155987af53e37b120ec736ef0266b8
DanielDlc <dlc.engcomputacao@gmail.com>
2021-09-30 06:55:21 -0300

    passagem por parâmetro

---
+++ cpp03/passagem_por_parâmetro.cpp
@@ -0,0 +1,27 @@
+// passagem de parâmetro para uma função, passamos uma cópia do valor que você usa como argumento. 
+//um exemplo de um argumento de uma função f() é uma variável local em f() que é inicializada cada vez que f() é
+// chamada.
+
+#include <iostream>
+
+int f(int x)
+{
+    x = x+1;
+    return x;
+}
+
+int main() {
+    
+    int xx= 0;
+    std::cout << f(xx) <<std::endl; // escreve:1
+    std::cout << xx <<std::endl;  // escreve: 0; f() não altera xx
+
+    std::cout <<"---------------------------" << std::endl;
+
+    int yy = 7;
+    std::cout << f(yy) <<std::endl; // escreve:8
+    std::cout << yy <<std::endl;  // escreve: 7; f() não altera yy
+
+    return 0;
+
+}
