1f993f9cbd3d199ae2ec2e255c895a4884c7fd8b
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2022-03-11 08:28:51 -0300

    condicionais if else

--- 02_condicionais/02_fi_else.cpp
+++ 02_condicionais/02_fi_else.cpp
@@ -1,7 +1,8 @@
+// estrutura condicionais if else
+
 #include <iostream>
 
 int main() {
-    // instruções if simples com else
     int i = 2;
     if (i > 2) {
         std::cout << i << "é maior que 2 \n";
