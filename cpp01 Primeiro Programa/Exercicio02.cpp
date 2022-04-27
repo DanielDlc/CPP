8dcbbc46cbf4d3209c0b662db588ad10c6767523
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2022-04-27 08:00:25 -0300

    olá mundo

--- cpp01 Primeiro Programa/Exercicio02.cpp
+++ cpp01 Primeiro Programa/Exercicio02.cpp
@@ -1,13 +1,6 @@
-/* No Código do exercício 02 vamos mostrar uma saída de dados
-° Criar um comentário de uma linha de código com //
-° Criar um comentário de várias linhas de código
-/**/ 
-/*Explicando e descrevendo o que faz e qual objetivo deste programa
-° Aumentar e Diminuir o texto de seu CódigoFonte*/
+// iniciando C++ 
 
 #include < iostream> 
-// Daniel Louro Costa
-// 14 de setembro de 2020
 
 int main() // indica que iniciou uma função com parametro main.
 {
