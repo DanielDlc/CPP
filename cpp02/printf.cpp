7c9de932310825ac14e9a5f3d77f255d3f7ab632
Daniel Louro Costa <56986967+DanielDlc@users.noreply.github.com>
2022-05-03 06:28:56 -0300

    Printf

--- cpp02/printf.cpp
+++ cpp02/printf.cpp
@@ -1,20 +1,22 @@
+// usando printf
+
 #include <iostream>
 #include<string>
 
 int main()
 {
-	// { } é uma inicialização uniforme, permite tanto iniciar valores literais (int float, char)
-	// permite também iniciar valores escalares (vetores, enums etc.)
+	// { } Ã© uma inicializaÃ§Ã£o uniforme, permite tanto iniciar valores literais (int float, char)
+	// permite tambÃ©m iniciar valores escalares (vetores, enums etc.)
 	int NumeroInteiro{ 12 };
 	float NumeroReal{ 98.76 };
 	char Caractere{ 'd' };
 
-	// String é uma cadeira de caracteres, é um texto e deve  estar entre aspas duplas "Texto teste"
+	// String Ã© uma cadeira de caracteres, Ã© um texto e deve  estar entre aspas duplas "Texto teste"
 	// Para declarar uma string usamos std::string
 	std::string Texto{ "Minha primeira String" };
 	
 	// %d %f %c %s indica aquilo que vc quer colocar na string
-	// %.2f define o número de casas decimal para exibir na tela
+	// %.2f define o nÃºmero de casas decimal para exibir na tela
 	// variavel.c_std() cham
 	std::printf("Valor de um numero Inteiro = %d \n", NumeroInteiro);
 	std::printf("Valor de um numero Real = %.2f \n", NumeroReal);
