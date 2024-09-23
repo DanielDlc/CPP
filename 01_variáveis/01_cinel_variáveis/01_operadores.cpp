c33af0797fe93fff276e52fd6f107a1fa583db34
DanielDlc <dlc.engcomputacao@gmail.com>
2024-09-23 23:53:12 +0100

    operadores.cpp

---
+++ 01_variáveis/01_cinel_variáveis/01_operadores.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int numerosInteiros, numero1, numero2;
+    double numeroDecimais, num2;
+
+    numerosInteiros = numero1 + numero2;
+
+    cout << "soma das minhas variaveis " << numerosInteiros << endl;
+    cout << numero1 << " + " << numero2 << " = " << numerosInteiros << endl;
+
+  return 0;
+}
